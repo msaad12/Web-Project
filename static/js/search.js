a9e083730e02306f671e5134327121ff0b64a07f
Muhammad Saad <muhammadsaad905@gmail.com>
2018-04-19 21:24:32 +0500

    Project

---
+++ static/js/search.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1 && filter.length>0) {
+            li[i].classList.remove("search-results");
+        } else {
+            li[i].classList.add("search-results");
+
+        }
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
